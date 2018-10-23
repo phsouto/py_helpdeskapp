aab7207492049af8a96a5a57c0fc83fb06a0d4da
Pedro H. G. Souto <phgsouto@gmail.com>
2018-10-22 21:12:12 -0300

    basic app. login and registration forms.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import psycopg2
 import hashlib
 
@@ -7,5 +7,23 @@ app.config.from_pyfile('config.py')
 
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return 'Hello!'
+    data = {'title': 'Home'}
+    return render_template('home.html', data=data)
+
+
+@app.route('/login')
+def login():
+    data = {'title': 'Login'}
+    return render_template('login.html', data=data)
+
+
+@app.route('/register')
+def register():
+    data = {'title': 'Register'}
+    return render_template('register.html', data=data)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ db.py
@@ -0,0 +1,32 @@
+#
+# file: db.py
+# purpose: handles database interactions
+# created on: 2018-10-21
+#
+
+from instance import config
+import hashlib
+import psycopg2
+
+def connect():
+    return psycopg2.connect(host =config.DATABASE_SERVER,
+                            dbname = config.DATABASE_NAME,
+                            user = config.DATABASE_USER,
+                            password = config.DATABASE_PWD)        
+
+
+def add_user(username, password):
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute(f"insert into users(name, passwd) values ('{username}', '{password}')")
+    conn.commit()
+    conn.close()
+
+
+def hash(stuff):
+    m = hashlib.sha256()
+    m.update(stuff.encode('UTF-8'))
+    return m.hexdigest()
+
+if __name__ == '__main__':
+    add_user('admin', hash('admin'))
